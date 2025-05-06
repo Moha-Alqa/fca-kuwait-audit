dcc3698f333f4d49b2c1b0dcef02035437307dfd
Mohammad Alqahtani <91021447+Moha-Alqa@users.noreply.github.com>
2025-05-07 01:48:00 +0700

    Changed Hero Image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import { ArrowRight } from "lucide-react";
 const Hero = () => {
   return (
     <section className="relative bg-blue-900 text-white">
-      <div className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1488972685288-c3fd157d7c7a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=80')] bg-cover bg-center opacity-10"></div>
+      <div className="absolute inset-0 bg-[url('https://t4.ftcdn.net/jpg/06/21/24/03/360_F_621240399_5hM5KCFFTRAZ2qB0U9tApz2SuGfEPEfZ.jpg"></div>
       
       <div className="relative container mx-auto px-4 py-24 sm:px-6 sm:py-32 lg:py-40">
         <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl mb-6">
